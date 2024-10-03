c4d071b238bb778ce19ffd13ce659adf62e55bdb
kimberli <kimberlizhong@gmail.com>
2024-10-03 00:01:35 -0700

    log errors in tests

--- src/web/__mocks__/logger.ts
+++ src/web/__mocks__/logger.ts
@@ -2,5 +2,6 @@ export const createLogger = jest.fn(() => ({
   debug: jest.fn(),
   info: jest.fn(),
   warn: jest.fn(),
-  error: jest.fn(),
+  // eslint-disable-next-line no-console
+  error: console.error,
 }));
