718eb45fa281cef802dea4b2f93d1608daf29c03
kimberli <kimberlizhong@gmail.com>
2024-10-08 10:36:50 -0700

    rm comments

--- src/web/__mocks__/supabase.ts
+++ src/web/__mocks__/supabase.ts
@@ -7,17 +7,3 @@ export const supabaseMock = {
     signOut: jest.fn().mockResolvedValue({ error: null }),
   },
 };
-
-// const supabaseMock = {
-//   ...createSupabaseMock(),
-//   resetMock: () => {
-//     this = {...createSupabaseMock(), this.resetMock};
-//   }
-// };
-
-// export const resetSupabaseMock = () => {
-//   supabaseMock = createSupabaseMock();
-// };
-
-// // Usage in your tests
-// export { supabaseMock };
