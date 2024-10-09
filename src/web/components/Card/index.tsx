3e3585ce8d65dffebbe9afe5dce605860a682165
kimberli <kimberlizhong@gmail.com>
2024-10-08 23:31:51 -0700

    adjust border radius

--- src/web/components/Card/index.tsx
+++ src/web/components/Card/index.tsx
@@ -3,7 +3,7 @@ import { Card, CardProps } from "@nextui-org/react";
 import { cn } from "@/utils/cn";
 
 const CurioCard: React.FC<CardProps> = ({ className, ...props }: CardProps) => {
-  return <Card {...props} className={cn("rounded-sm p-2", className)} />;
+  return <Card {...props} className={cn("rounded p-4", className)} />;
 };
 
 export default CurioCard;
