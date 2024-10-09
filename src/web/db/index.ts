d6c8007cd896db2dfc3872a42ad987b1e2eb26c2
kimberli <kimberlizhong@gmail.com>
2024-10-08 22:48:24 -0700

    use connection pool from node-postgres

--- src/web/db/index.ts
+++ src/web/db/index.ts
@@ -1,11 +1,14 @@
 /* eslint-disable no-restricted-imports */
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
 
 import * as schema from "@/db/schema";
 
-const client = postgres(process.env.POSTGRES_URL!);
-export const db = drizzle(client, { schema });
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL,
+});
+
+export const db = drizzle(pool, { schema });
 
 export { eq, gt, gte, lt, lte, ne } from "drizzle-orm";
 export type { PgColumn, SelectedFields } from "drizzle-orm/pg-core";
