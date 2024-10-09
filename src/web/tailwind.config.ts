5eb84b79269b4530580f2f9dab16aef899dbaf31
kimberli <kimberlizhong@gmail.com>
2024-10-08 23:31:51 -0700

    adjust border radius

--- src/web/tailwind.config.ts
+++ src/web/tailwind.config.ts
@@ -62,9 +62,9 @@ const config: Config = {
             "0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)",
         },
         radius: {
-          small: "0.125rem",
-          medium: "0.125rem",
-          large: "0.125rem",
+          small: "0.25rem",
+          medium: "0.25rem",
+          large: "0.25rem",
         },
       },
       themes: {
