f0d0c6aac55fec2ad309e465bb6ce8bfad5e0e7e
kimberli <kimberlizhong@gmail.com>
2025-01-09 18:27:59 -0800

    fix modal positioning

--- src/web/components/Modal/index.tsx
+++ src/web/components/Modal/index.tsx
@@ -16,8 +16,9 @@ const CurioModal: React.FC<CurioModalProps> = ({
   return (
     <Modal
       classNames={{
-        backdrop: "backdrop-opacity-80 backdrop-brightness-50 backdrop-blur-sm	",
-        base: "rounded",
+        backdrop: "backdrop-opacity-80 backdrop-brightness-50 backdrop-blur-sm",
+        base: "rounded max-h-[90vh] my-4",
+        wrapper: "overflow-y-auto py-4 items-center",
         body: "pb-4",
         closeButton: "hover:bg-background active:bg-background",
       }}
