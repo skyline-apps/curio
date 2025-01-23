ef860d4df477acfa59c3e875c6247c62cb0604f1
kimberli <kimberlizhong@gmail.com>
2025-01-22 17:15:13 -0800

    pass through input custom styles

--- src/web/components/ui/Input/index.tsx
+++ src/web/components/ui/Input/index.tsx
@@ -1,13 +1,15 @@
 import { Input, InputProps } from "@nextui-org/react";
 import React from "react";
 
-const CurioInput: React.FC<InputProps> = (props: InputProps) => {
+const CurioInput: React.FC<InputProps> = ({
+  className,
+  ...props
+}: InputProps) => {
   return (
     <Input
       size="sm"
       classNames={{
-        inputWrapper:
-          "bg-default-50 dark:bg-default hover:bg-default-75 data-[hover=true]:bg-default-75 group-data-[focus=true]:bg-default-75 dark:hover:bg-default-700 dark:group-data-[focus=true]:bg-default-700 dark:data-[hover=true]:bg-default-700",
+        inputWrapper: `bg-default-50 dark:bg-default hover:bg-default-75 data-[hover=true]:bg-default-75 group-data-[focus=true]:bg-default-75 dark:hover:bg-default-700 dark:group-data-[focus=true]:bg-default-700 dark:data-[hover=true]:bg-default-700 ${className}`,
       }}
       {...props}
     />
