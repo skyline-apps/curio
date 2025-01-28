39bdd267dc9066158c28e06624cb6921a86c45bf
kimberli <kimberlizhong@gmail.com>
2025-01-28 00:55:12 -0800

    update db to move metadata to profileItems

---
+++ src/web/db/migrations/0006_non-null title.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "profile_items" ALTER COLUMN "title" SET NOT NULL;
--- src/web/utils/test/provider.ts
+++ src/web/utils/test/provider.ts
@@ -5,18 +5,10 @@ export let testDb: Awaited<ReturnType<typeof createTestDb>>;
 
 // Override the database instance for tests
 jest.mock("@/db/index", () => ({
-  __esModule: true,
+  ...jest.requireActual("@/db/index"),
   get db() {
     return testDb.db;
   },
-  eq: jest.requireActual("drizzle-orm").eq,
-  gt: jest.requireActual("drizzle-orm").gt,
-  gte: jest.requireActual("drizzle-orm").gte,
-  lt: jest.requireActual("drizzle-orm").lt,
-  lte: jest.requireActual("drizzle-orm").lte,
-  ne: jest.requireActual("drizzle-orm").ne,
-  sql: jest.requireActual("drizzle-orm").sql,
-  and: jest.requireActual("drizzle-orm").and,
 }));
 
 beforeAll(async () => {
