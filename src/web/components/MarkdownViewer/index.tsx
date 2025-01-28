0eed1732e73ea94991b6072610a9c22e979aa78a
kimberli <kimberlizhong@gmail.com>
2025-01-27 17:57:22 -0800

    correct text color

--- src/web/components/MarkdownViewer/index.tsx
+++ src/web/components/MarkdownViewer/index.tsx
@@ -4,7 +4,7 @@ interface MarkdownViewerProps extends Options {}
 
 const MarkdownViewer = (options: MarkdownViewerProps): JSX.Element => {
   return (
-    <div className="prose prose-slate dark:prose-invert max-w-none">
+    <div className="prose prose-slate max-w-none [&_*]:!text-default-foreground hover:prose-a:!text-primary dark:prose-invert">
       <Markdown {...options} />
     </div>
   );
