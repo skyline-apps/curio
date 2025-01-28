011036074cde759c2a63685ebfabaec0a6ad1dbc
kimberli <kimberlizhong@gmail.com>
2025-01-27 17:48:36 -0800

    style markdown

--- src/web/components/MarkdownViewer/index.tsx
+++ src/web/components/MarkdownViewer/index.tsx
@@ -3,7 +3,11 @@ import Markdown, { Options } from "react-markdown";
 interface MarkdownViewerProps extends Options {}
 
 const MarkdownViewer = (options: MarkdownViewerProps): JSX.Element => {
-  return <Markdown {...options} />;
+  return (
+    <div className="prose prose-slate dark:prose-invert max-w-none">
+      <Markdown {...options} />
+    </div>
+  );
 };
 
 export default MarkdownViewer;
