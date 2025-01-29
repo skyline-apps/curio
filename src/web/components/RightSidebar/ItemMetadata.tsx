229303c65a31173f3e5e75e4cac797077218d3e0
kimberli <kimberlizhong@gmail.com>
2025-01-28 23:51:57 -0800

    hide right sidebar on mobile

--- src/web/components/RightSidebar/ItemMetadata.tsx
+++ src/web/components/RightSidebar/ItemMetadata.tsx
@@ -23,11 +23,11 @@ const ItemMetadata: React.FC<ItemMetadataProps> = ({
           />
         </div>
       )}
-      <div className="p-2">
+      <div className="p-4">
         <Link className="hover:underline" href={`/items/${item.slug}`}>
           <h2>{metadata.title}</h2>
         </Link>
-        <div className="flex flex-row justify-between gap-2 text-sm text-secondary-300">
+        <div className="flex flex-row justify-between gap-2 text-sm text-secondary-300 dark:text-secondary-600">
           {metadata.author && <p>{metadata.author}</p>}
           {metadata.publishedAt && (
             <p>{new Date(metadata.publishedAt).toLocaleDateString()}</p>
