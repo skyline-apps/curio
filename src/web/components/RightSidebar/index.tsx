9a667cafa186fac974f91bb2f8cf6de956b57887
kimberli <kimberlizhong@gmail.com>
2025-01-28 23:51:57 -0800

    hide right sidebar on mobile

--- src/web/components/RightSidebar/index.tsx
+++ src/web/components/RightSidebar/index.tsx
@@ -21,20 +21,32 @@ const RightSidebar: React.FC = () => {
   return (
     <aside
       className={cn(
-        "h-screen border-l-1 border-divider transition-all duration-300 ease-in-out absolute right-0 md:relative bg-background",
-        sidebarOpen ? "w-72" : "w-16",
+        "h-screen border-l-1 border-divider transition-all duration-300 ease-in-out absolute right-0 md:relative bg-background-400 shadow-lg",
+        sidebarOpen ? "w-80" : "w-0 md:w-16",
       )}
     >
       <div className="flex flex-col justify-between h-full">
-        <div className="grow">
-          {sidebarOpen && <ItemMetadata item={currentItem?.item} />}
+        <div className="grow overflow-hidden">
+          <div
+            className={cn(
+              "w-80 transition-all duration-300 ease-in-out transform",
+              sidebarOpen
+                ? "translate-x-0 opacity-100"
+                : "translate-x-full opacity-0",
+            )}
+          >
+            {sidebarOpen && <ItemMetadata item={currentItem?.item} />}
+          </div>
         </div>
         <Button
           isIconOnly
           variant="light"
           onPress={toggleSidebar}
           aria-label={sidebarOpen ? "Collapse sidebar" : "Expand sidebar"}
-          className={cn("flex-none w-10 m-2 flex")}
+          className={cn(
+            "flex-none w-10 m-2 flex",
+            sidebarOpen ? "" : "absolute right-0 bottom-0 md:relative",
+          )}
         >
           <Icon
             icon={
