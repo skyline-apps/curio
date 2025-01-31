baed788b67e4e672941a523555f7eeb70686b3c6
kimberli <kimberlizhong@gmail.com>
2025-01-30 17:50:05 -0800

    add new item modal

--- src/web/components/LeftSidebar/index.tsx
+++ src/web/components/LeftSidebar/index.tsx
@@ -7,6 +7,7 @@ import {
   HiPlus,
 } from "react-icons/hi2";
 
+import NewItemModal from "@/components/NewItemModal";
 import Button from "@/components/ui/Button";
 import Icon from "@/components/ui/Icon";
 import { useLogout } from "@/hooks/useLogout";
@@ -23,6 +24,7 @@ const LeftSidebar: React.FC = () => {
   const [sidebarOpen, setSidebarOpen] = useState<boolean>(true);
   const pathname = usePathname();
   const [selectedKey, setSelectedKey] = useState<string>(SidebarKey.NONE);
+  const [showNewItemModal, setShowNewItemModal] = useState<boolean>(false);
 
   const handleLogout = useLogout();
 
@@ -44,65 +46,69 @@ const LeftSidebar: React.FC = () => {
   };
 
   return (
-    <aside
-      className={cn(
-        "flex flex-col justify-between h-screen border-r-1 border-divider transition-all duration-300 ease-in-out",
-        "w-16 md:w-64",
-        { "overflow-hidden w-0 md:w-16": !sidebarOpen },
-      )}
-    >
-      <div className="grow">
-        <SidebarHeader sidebarOpen={sidebarOpen} />
-        <NavigationMenu
-          sidebarOpen={sidebarOpen}
-          selectedKey={selectedKey}
-          onNavigation={handleNavigation}
-        />
-      </div>
-      <Button
-        className={cn("m-2 min-w-10", {
-          "self-center": !sidebarOpen,
-          "hidden md:flex": !sidebarOpen,
-        })}
-        isIconOnly={!sidebarOpen}
-        onPress={
-          () => null // TODO: Implement saving pages from app
-        }
-      >
-        <Icon icon={<HiPlus />} />
-        <span className="hidden md:block">{sidebarOpen && "Add new"}</span>
-      </Button>
-      <div
+    <>
+      <aside
         className={cn(
-          "flex items-center",
-          sidebarOpen ? "flex-col md:flex-row gap-2" : "flex-col",
+          "flex flex-col justify-between h-screen border-r-1 border-divider transition-all duration-300 ease-in-out",
+          "w-16 md:w-64",
+          { "overflow-hidden w-0 md:w-16": !sidebarOpen },
         )}
       >
-        <UserMenu
-          user={user}
-          sidebarOpen={sidebarOpen}
-          selectedKey={selectedKey}
-          onLogout={handleLogout}
-          onNavigate={handleNavigation}
-        />
+        <div className="grow">
+          <SidebarHeader sidebarOpen={sidebarOpen} />
+          <NavigationMenu
+            sidebarOpen={sidebarOpen}
+            selectedKey={selectedKey}
+            onNavigation={handleNavigation}
+          />
+        </div>
         <Button
-          isIconOnly
-          variant="faded"
-          onPress={toggleSidebar}
-          aria-label={sidebarOpen ? "Collapse sidebar" : "Expand sidebar"}
+          className={cn("m-2 min-w-10", {
+            "self-center": !sidebarOpen,
+            "hidden md:flex": !sidebarOpen,
+          })}
+          isIconOnly={!sidebarOpen}
+          onPress={() => setShowNewItemModal(true)}
+        >
+          <Icon icon={<HiPlus />} />
+          <span className="hidden md:block">{sidebarOpen && "Add new"}</span>
+        </Button>
+        <div
           className={cn(
-            "flex-none w-10 z-10 m-2",
-            sidebarOpen ? "" : "absolute left-0 bottom-0 md:relative",
+            "flex items-center",
+            sidebarOpen ? "flex-col md:flex-row gap-2" : "flex-col",
           )}
         >
-          <Icon
-            icon={
-              sidebarOpen ? <HiChevronDoubleLeft /> : <HiChevronDoubleRight />
-            }
+          <UserMenu
+            user={user}
+            sidebarOpen={sidebarOpen}
+            selectedKey={selectedKey}
+            onLogout={handleLogout}
+            onNavigate={handleNavigation}
           />
-        </Button>
-      </div>
-    </aside>
+          <Button
+            isIconOnly
+            variant="faded"
+            onPress={toggleSidebar}
+            aria-label={sidebarOpen ? "Collapse sidebar" : "Expand sidebar"}
+            className={cn(
+              "flex-none w-10 z-10 m-2",
+              sidebarOpen ? "" : "absolute left-0 bottom-0 md:relative",
+            )}
+          >
+            <Icon
+              icon={
+                sidebarOpen ? <HiChevronDoubleLeft /> : <HiChevronDoubleRight />
+              }
+            />
+          </Button>
+        </div>
+      </aside>
+      <NewItemModal
+        isOpen={showNewItemModal}
+        onClose={() => setShowNewItemModal(false)}
+      />
+    </>
   );
 };
 
