1ae3e2f71a96ef67ba8451382d13b4732e5cafe2
kimberli <kimberlizhong@gmail.com>
2025-02-04 00:38:58 -0800

    convert to listbox

--- src/web/components/Items/ItemCard/index.tsx
+++ src/web/components/Items/ItemCard/index.tsx
@@ -1,53 +1,37 @@
 import Link from "next/link";
-import { useContext } from "react";
 
 import ItemActions from "@/components/RightSidebar/ItemActions";
-import { CurrentItemContext } from "@/providers/CurrentItemProvider";
 import type { ItemMetadata } from "@/providers/ItemsProvider";
-import { cn } from "@/utils/cn";
 
 interface ItemCardProps {
   item: ItemMetadata;
 }
 
 const ItemCard: React.FC<ItemCardProps> = ({ item }: ItemCardProps) => {
-  const { currentItem, selectItem } = useContext(CurrentItemContext);
   const getHostname = (url: string): string =>
     new URL(url).hostname.replace(/www./, "");
 
   return (
-    <div
-      key={item.id}
-      className={cn(
-        "flex flex-row bg-background-400 pl-4 pr-1 py-1 h-16 rounded-sm overflow-hidden hover:bg-background-300 group",
-        currentItem?.id === item.id && "bg-background-300",
-      )}
-      onClick={() => selectItem(item.slug)}
-      data-active={currentItem?.id === item.id ? true : undefined}
-    >
-      <div className="flex flex-row gap-2 justify-between items-start w-full">
-        <div className="grow h-full block overflow-hidden">
-          <div className="flex flex-row gap-2 items-center">
-            <Link
-              href={`/items/${item.slug}`}
-              className="text-sm text-foreground hover:underline"
-              onClick={(ev) => ev.stopPropagation()}
-            >
-              {item.metadata.title}
-            </Link>
-            <p className="text-xs text-secondary-600">
-              {getHostname(item.url)}
-            </p>
-          </div>
-          <p className="text-xs text-secondary text-wrap">
-            {item.metadata.description}
-          </p>
+    <div className="flex flex-row gap-2 justify-between items-start w-full pb-1">
+      <div className="grow h-full block overflow-hidden">
+        <div className="flex flex-row gap-2 items-center">
+          <Link
+            href={`/items/${item.slug}`}
+            className="text-sm text-foreground hover:underline"
+            onClick={(ev) => ev.stopPropagation()}
+          >
+            {item.metadata.title}
+          </Link>
+          <p className="text-xs text-secondary-600">{getHostname(item.url)}</p>
         </div>
-        <ItemActions
-          item={item}
-          className="invisible group-hover:visible group-data-[active=true]:visible"
-        />
+        <p className="text-xs text-secondary text-wrap">
+          {item.metadata.description}
+        </p>
       </div>
+      <ItemActions
+        item={item}
+        className="invisible group-hover:visible group-data-[active=true]:visible"
+      />
     </div>
   );
 };
--- src/web/components/Items/ItemList/index.tsx
+++ src/web/components/Items/ItemList/index.tsx
@@ -1,6 +1,7 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 import ItemCard from "@/components/Items/ItemCard";
+import { Listbox, ListboxItem } from "@/components/ui/Listbox";
 import { CurrentItemContext } from "@/providers/CurrentItemProvider";
 import { ItemsContext } from "@/providers/ItemsProvider";
 
@@ -8,18 +9,57 @@ interface ItemListProps {}
 
 const ItemList: React.FC<ItemListProps> = () => {
   const { items } = useContext(ItemsContext);
-  const { unselectItem } = useContext(CurrentItemContext);
+  const { selectedItems, selectItems, unselectItems } =
+    useContext(CurrentItemContext);
+  const [windowHeight, setWindowHeight] = useState<number>(window.innerHeight);
 
   useEffect(() => {
-    unselectItem();
+    unselectItems();
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowHeight(window.innerHeight);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const handleSelectionChange = (keys: Selection): void => {
+    selectItems(Array.from(keys) as string[]);
+  };
+
   return (
-    <div className="flex flex-col gap-1">
+    <Listbox
+      variant="light"
+      aria-label="Item list"
+      selectedKeys={new Set(selectedItems)}
+      onSelectionChange={handleSelectionChange}
+      selectionMode="multiple"
+      autoFocus="first"
+      hideSelectedIcon
+      hideEmptyContent
+      isVirtualized
+      virtualization={{
+        maxListboxHeight: windowHeight - 100,
+        itemHeight: 63,
+      }}
+      classNames={{ base: "overflow-hidden p-0" }}
+    >
       {items.map((item) => (
-        <ItemCard key={item.id} item={item} />
+        <ListboxItem
+          key={item.slug}
+          classNames={{
+            base: "h-[63px] flex items-start border-2 border-background p-2 bg-background-400 rounded-sm overflow-hidden hover:bg-background-300 group data-[selected=true]:bg-background-300 data-[focus=true]:outline-none data-[focus-visible=true]:outline-none",
+          }}
+          shouldHighlightOnFocus
+          textValue={item.metadata.title}
+        >
+          <ItemCard item={item} />
+        </ListboxItem>
       ))}
-    </div>
+    </Listbox>
   );
 };
 
