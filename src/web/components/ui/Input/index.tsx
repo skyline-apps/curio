6509c85c947a25e67063000525172527585ee23e
kimberli <kimberlizhong@gmail.com>
2025-02-05 19:16:37 -0800

    fix input color on dark

--- src/web/components/ui/Input/index.tsx
+++ src/web/components/ui/Input/index.tsx
@@ -9,7 +9,7 @@ const CurioInput: React.FC<InputProps> = ({
     <Input
       size="sm"
       classNames={{
-        inputWrapper: `bg-default-50 dark:bg-default hover:bg-default-75 data-[hover=true]:bg-default-75 group-data-[focus=true]:bg-default-75 dark:hover:bg-default-700 dark:group-data-[focus=true]:bg-default-700 dark:data-[hover=true]:bg-default-700 ${className}`,
+        inputWrapper: `bg-default-50 dark:bg-default-950 hover:bg-default-75 data-[hover=true]:bg-default-75 group-data-[focus=true]:bg-default-75 dark:hover:bg-default-975 dark:group-data-[focus=true]:bg-default-975 dark:data-[hover=true]:bg-default-975 ${className}`,
         input: "placeholder:text-secondary-400",
       }}
       {...props}
