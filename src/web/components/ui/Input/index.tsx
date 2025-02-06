ea8ee76ca9bde9a80cc7dd5726b03a6553984590
kimberli <kimberlizhong@gmail.com>
2025-02-05 19:44:52 -0800

    change input placeholder color

--- src/web/components/ui/Input/index.tsx
+++ src/web/components/ui/Input/index.tsx
@@ -10,7 +10,8 @@ const CurioInput: React.FC<InputProps> = ({
       size="sm"
       classNames={{
         inputWrapper: `bg-default-50 dark:bg-default-950 hover:bg-default-75 data-[hover=true]:bg-default-75 group-data-[focus=true]:bg-default-75 dark:hover:bg-default-975 dark:group-data-[focus=true]:bg-default-975 dark:data-[hover=true]:bg-default-975 ${className}`,
-        input: "placeholder:text-secondary-400",
+        input:
+          "placeholder:text-secondary-400 dark:placeholder:text-secondary-900",
       }}
       {...props}
     />
