41794c0f757bd194890a6ffaf5b75ed7c5703b49
kimberli <kimberlizhong@gmail.com>
2025-02-11 12:46:57 -0800

    add start-offset to rendered nodes

--- src/web/components/Article/MarkdownViewer.tsx
+++ src/web/components/Article/MarkdownViewer.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { type ComponentPropsWithoutRef } from "react";
+import type { Components } from "react-markdown";
 import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
 import {
   CreateOrUpdateHighlightResponse,
@@ -23,11 +25,75 @@ interface MarkdownViewerProps {
   children?: string;
 }
 
+interface MarkdownNode {
+  position?: {
+    start: {
+      offset: number;
+    };
+  };
+  type?: string;
+  value?: string;
+}
+
+type MarkdownProps<T extends keyof JSX.IntrinsicElements> =
+  ComponentPropsWithoutRef<T> & {
+    node?: MarkdownNode;
+  };
+
+const createComponentWithOffset = <T extends keyof JSX.IntrinsicElements>(
+  tag: T,
+): React.FC<MarkdownProps<T>> => {
+  const MarkdownComponent = ({
+    node,
+    children,
+    ...rest
+  }: MarkdownProps<T>): JSX.Element => {
+    const startOffset = node?.position?.start?.offset;
+    return React.createElement(
+      tag,
+      {
+        "data-start-offset": startOffset,
+        ...rest,
+      },
+      children,
+    );
+  };
+  return MarkdownComponent;
+};
+
+const components = {
+  h1: createComponentWithOffset("h1"),
+  h2: createComponentWithOffset("h2"),
+  h3: createComponentWithOffset("h3"),
+  h4: createComponentWithOffset("h4"),
+  h5: createComponentWithOffset("h5"),
+  h6: createComponentWithOffset("h6"),
+  p: createComponentWithOffset("p"),
+  ul: createComponentWithOffset("ul"),
+  ol: createComponentWithOffset("ol"),
+  li: createComponentWithOffset("li"),
+  blockquote: createComponentWithOffset("blockquote"),
+  code: createComponentWithOffset("code"),
+  pre: createComponentWithOffset("pre"),
+  em: createComponentWithOffset("em"),
+  strong: createComponentWithOffset("strong"),
+  a: createComponentWithOffset("a"),
+  img: createComponentWithOffset("img"),
+  del: createComponentWithOffset("del"),
+  hr: createComponentWithOffset("hr"),
+  table: createComponentWithOffset("table"),
+  thead: createComponentWithOffset("thead"),
+  tbody: createComponentWithOffset("tbody"),
+  tr: createComponentWithOffset("tr"),
+  td: createComponentWithOffset("td"),
+  th: createComponentWithOffset("th"),
+} as Components;
+
 const MarkdownViewer: React.FC<MarkdownViewerProps> = ({
   readingProgress,
   onProgressChange,
-  highlights,
-  onHighlight,
+  highlights: _highlights,
+  onHighlight: _onHighlight,
   className,
   children,
 }) => {
@@ -45,7 +111,9 @@ const MarkdownViewer: React.FC<MarkdownViewerProps> = ({
         className,
       )}
     >
-      <ReactMarkdown>{children || ""}</ReactMarkdown>
+      <ReactMarkdown remarkPlugins={[remarkGfm]} components={components}>
+        {children || ""}
+      </ReactMarkdown>
     </div>
   );
 };
