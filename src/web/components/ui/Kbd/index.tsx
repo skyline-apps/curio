2e237dcdac68429eff6bffda55c3b2dc8db0181b
kimberli <kimberlizhong@gmail.com>
2025-02-12 18:48:30 -0800

    fix coloring on kbd

--- src/web/components/ui/Kbd/index.tsx
+++ src/web/components/ui/Kbd/index.tsx
@@ -1,1 +1,15 @@
-export { Kbd, type KbdKey } from "@heroui/react";
+import { Kbd, type KbdProps } from "@heroui/react";
+import { cn } from "utils/cn";
+
+export { type KbdKey } from "@heroui/react";
+
+interface CurioKbdProps extends KbdProps {}
+
+const CurioKbd = ({
+  className,
+  ...props
+}: CurioKbdProps): React.ReactElement => {
+  return <Kbd className={cn("bg-background-700", className)} {...props}></Kbd>;
+};
+
+export default CurioKbd;
