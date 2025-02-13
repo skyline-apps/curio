9d2e4ef841c5229f493af625fb184c1c125a6efe
kimberli <kimberlizhong@gmail.com>
2025-02-12 18:48:30 -0800

    fix coloring on kbd

--- src/web/components/KeyboardShortcuts/index.tsx
+++ src/web/components/KeyboardShortcuts/index.tsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 
-import { Kbd, type KbdKey } from "@/components/ui/Kbd";
+import Kbd, { type KbdKey } from "@/components/ui/Kbd";
 import Modal, {
   ModalBody,
   ModalContent,
