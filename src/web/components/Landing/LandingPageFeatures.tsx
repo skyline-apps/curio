d20e7c6d1c9fcacd5883132bf445a40434785acf
kimberli <kimberlizhong@gmail.com>
2025-02-19 01:08:10 -0800

    add sample labels to landing page

--- src/web/components/Landing/LandingPageFeatures.tsx
+++ src/web/components/Landing/LandingPageFeatures.tsx
@@ -1,8 +1,12 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
+import Labels, { type Label } from "@/components/Labels";
 import { cn } from "@/utils/cn";
 
-interface FeatureProps {
+import { sampleLabels } from "./sampleData";
+
+interface FeatureProps extends React.PropsWithChildren {
   title: string;
   description: string;
   isReversed?: boolean;
@@ -12,6 +16,7 @@ const Feature: React.FC<FeatureProps> = ({
   title,
   description,
   isReversed,
+  children,
 }: FeatureProps) => (
   <div
     className={cn(
@@ -30,11 +35,30 @@ const Feature: React.FC<FeatureProps> = ({
       <h4 className="italic text-lg">{title}</h4>
       <p className="mt-2 text-secondary">{description}</p>
     </div>
-    <div className="bg-background h-40 w-60 shrink-0"></div>
+    <div className="bg-background h-40 w-60 shrink-0 p-2 overflow-hidden">
+      {children}
+    </div>
   </div>
 );
 
 const LandingPageFeatures: React.FC = () => {
+  const [labels, setLabels] = useState<Label[]>(sampleLabels);
+
+  const handleAddLabel = (label: Omit<Label, "id">): void => {
+    setLabels([
+      ...labels,
+      {
+        color: label.color,
+        name: label.name.substring(0, 20),
+        id: crypto.randomUUID(),
+      },
+    ]);
+  };
+
+  const handleDeleteLabel = (id: string): void => {
+    setLabels(labels.filter((label) => label.id !== id));
+  };
+
   return (
     <>
       <Feature
@@ -45,7 +69,14 @@ const LandingPageFeatures: React.FC = () => {
       <Feature
         title="Organize"
         description="Your ideas, beautifully organized. Tag, favorite, highlight, and add notes to build your personal library of insights."
-      />
+      >
+        <Labels
+          mode="create"
+          labels={labels}
+          onAdd={handleAddLabel}
+          onDelete={handleDeleteLabel}
+        />
+      </Feature>
       <Feature
         title="Focus"
         description="Read on your terms, free from distractions. Dive into articles in a clean, streamlined markdown viewer that works online and offline."
