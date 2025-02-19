7f3f43b4f5ce180c1da6c048f1f06013ad38da64
kimberli <kimberlizhong@gmail.com>
2025-02-19 01:08:10 -0800

    add sample labels to landing page

---
+++ src/web/components/Landing/sampleData.ts
@@ -0,0 +1,24 @@
+import { COLOR_PALETTE } from "@/components/ui/Chip";
+
+export const sampleLabels = [
+  {
+    id: "1",
+    name: "Essays",
+    color: COLOR_PALETTE[0],
+  },
+  {
+    id: "2",
+    name: "News",
+    color: COLOR_PALETTE[1],
+  },
+  {
+    id: "3",
+    name: "Travel",
+    color: COLOR_PALETTE[2],
+  },
+  {
+    id: "4",
+    name: "Book reviews",
+    color: COLOR_PALETTE[3],
+  },
+];
