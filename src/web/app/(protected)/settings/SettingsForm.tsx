46e063919e1603dc968634584964cf394168d394
kimberli <kimberlizhong@gmail.com>
2025-03-03 11:03:10 -0800

    add contact info

--- src/web/app/(protected)/settings/SettingsForm.tsx
+++ src/web/app/(protected)/settings/SettingsForm.tsx
@@ -13,6 +13,18 @@ const SettingsForm: React.FC = () => {
       <LabelSettings />
       <h2 className="text-lg my-2">Display</h2>
       <UpdateUserSettings />
+      <div className="flex flex-col h-full justify-end items-center text-xs text-secondary py-4">
+        <p>
+          Questions? Contact us at{" "}
+          <a className="hover:underline" href="mailto:team@curi.ooo">
+            team@curi.ooo
+          </a>
+          .
+        </p>
+        {process.env.VERCEL_GIT_COMMIT_SHA && (
+          <p>Build number: {process.env.VERCEL_GIT_COMMIT_SHA}</p>
+        )}
+      </div>
     </>
   );
 };
