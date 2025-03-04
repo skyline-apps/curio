5bf974f1e1683f581cbc4e38c7853b89c08bf967
kimberli <kimberlizhong@gmail.com>
2025-03-03 20:12:34 -0800

    change switch background color

--- src/web/components/ui/Switch/index.tsx
+++ src/web/components/ui/Switch/index.tsx
@@ -1,7 +1,13 @@
 import { Switch, SwitchProps } from "@heroui/react";
 
 const CurioSwitch: React.FC<SwitchProps> = (props: SwitchProps) => {
-  return <Switch size="sm" {...props} />;
+  return (
+    <Switch
+      size="sm"
+      classNames={{ wrapper: "bg-default-50 dark:bg-default-950" }}
+      {...props}
+    />
+  );
 };
 
 export { CurioSwitch as Switch };
