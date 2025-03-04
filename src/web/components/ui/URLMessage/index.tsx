e6c3c5eda4e6576f6ea3573e0e3a4693b8dc2add
kimberli <kimberlizhong@gmail.com>
2025-03-03 17:42:13 -0800

    adjust error components

--- src/web/components/ui/URLMessage/index.tsx
+++ src/web/components/ui/URLMessage/index.tsx
@@ -14,17 +14,7 @@ const URLMessage: React.FC<URLMessageProps> = ({
   const content = success || error || message || fallbackMessage;
   return (
     <div className="flex flex-col gap-2 w-full max-w-md text-sm">
-      {success && (
-        <div className="text-foreground border-l-2 border-foreground px-4">
-          {success}
-        </div>
-      )}
-      {error && (
-        <div className="text-destructive-foreground border-l-2 border-destructive-foreground px-4">
-          {error}
-        </div>
-      )}
-      <div className="text-foreground border-l-2 px-4">{content}</div>
+      <div className="text-foreground px-4">{content}</div>
     </div>
   );
 };
