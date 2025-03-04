dc61e7bf2523c2a23fb0e358dd440a21463f7378
kimberli <kimberlizhong@gmail.com>
2025-03-03 18:24:10 -0800

    allow vercel bypass

--- src/script/importOmnivore.ts
+++ src/script/importOmnivore.ts
@@ -4,6 +4,7 @@ import unzipper from "unzipper";
 import fetch from "node-fetch";
 
 const API_HOSTNAME = process.env.API_HOSTNAME || "http://localhost:3000";
+const BYPASS = process.env.BYPASS || "";
 const API_KEY = process.env.API_KEY || "";
 const ITEMS_ENDPOINT = `${API_HOSTNAME}/api/v1/items`;
 const CONTENT_ENDPOINT = `${API_HOSTNAME}/api/v1/items/content`;
@@ -40,7 +41,7 @@ async function sendItems(items: any[]) {
         }));
         const response = await fetch(ITEMS_ENDPOINT, {
             method: "POST",
-            headers: { "Content-Type": "application/json", "x-api-key": API_KEY },
+            headers: { "Content-Type": "application/json", "x-api-key": API_KEY, 'x-vercel-protection-bypass': BYPASS },
             body: JSON.stringify({ items: batch }),
         });
 
@@ -54,7 +55,7 @@ async function sendItems(items: any[]) {
 async function sendItemContent(url: string, htmlContent: string, contentPath: string) {
     const response = await fetch(CONTENT_ENDPOINT, {
         method: "POST",
-        headers: { "Content-Type": "application/json", "x-api-key": API_KEY },
+        headers: { "Content-Type": "application/json", "x-api-key": API_KEY, 'x-vercel-protection-bypass': BYPASS },
         body: JSON.stringify({ htmlContent, url: convertUrl(url), skipMetadataExtraction: true }),
     });
 
