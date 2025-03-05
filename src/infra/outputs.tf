581dbd53c93cd36d8339be56822e4908b2867757
kimberli <kimberlizhong@gmail.com>
2025-03-05 13:03:10 -0800

    rename node, enable csi storage

--- src/infra/outputs.tf
+++ src/infra/outputs.tf
@@ -1,7 +1,27 @@
 output "gcp" {
-    value = module.gcp
+  value = module.gcp
 }
 
 output "gke" {
-    value = module.gke
+  value = module.gke
+}
+
+output "project_id" {
+  value = var.gcp_project_id
+}
+
+output "zone" {
+  value = module.gcp.zone
+}
+
+output "cluster_name" {
+  value = var.cluster_name
+}
+
+output "connection_string" {
+  value = module.gke._connection_string
+}
+
+output "persistent_volume_disk_size_gb" {
+  value = var.persistent_volume_disk_size_gb
 }
