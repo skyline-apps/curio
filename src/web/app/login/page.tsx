e5bf2907ff14b8292934e236767b13749b97d2d6
kimberli <kimberlizhong@gmail.com>
2025-03-16 18:24:45 -0700

    redirect when already logged in

--- src/web/app/login/page.tsx
+++ src/web/app/login/page.tsx
@@ -1,15 +1,26 @@
 "use client";
 
-import React from "react";
+import { useRouter } from "next/navigation";
+import React, { useContext } from "react";
 
 import EmailSignIn from "@/components/Auth/EmailSignIn";
 import GoogleSignIn from "@/components/Auth/GoogleSignIn";
+import Spinner from "@/components/ui/Spinner";
+import { UserContext } from "@/providers/UserProvider";
 import YarnDark from "@/public/assets/yarn_dark.svg";
 import YarnLight from "@/public/assets/yarn_light.svg";
 
 interface LoginPageProps {}
 
 const LoginPage: React.FC<LoginPageProps> = ({}: LoginPageProps) => {
+  const { user } = useContext(UserContext);
+  const router = useRouter();
+  if (user && user.id) {
+    // Already signed in; redirect to home
+    router.push("/home");
+    return <Spinner centered />;
+  }
+
   return (
     <div className="flex flex-col gap-4 my-16">
       <div className="flex flex-col items-center gap-2 w-64 mx-auto">
