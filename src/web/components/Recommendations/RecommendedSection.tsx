ad5928eb536383f8a0165f7ad732ba9f5b3c4e56
kimberli <kimberlizhong@gmail.com>
2025-03-21 16:46:02 -0700

    remove extra scrollbar

--- src/web/components/Recommendations/RecommendedSection.tsx
+++ src/web/components/Recommendations/RecommendedSection.tsx
@@ -61,7 +61,7 @@ const RecommendedSection: React.FC<RecommendedSectionProps> = ({
       <h2 className="text-lg text-secondary mb-2">
         {SECTION_HEADERS[sectionType]}
       </h2>
-      <div className="flex flex-row gap-2 overflow-x-auto">
+      <div className="flex flex-row gap-2 overflow-x-auto overflow-y-hidden">
         {!items.length ? (
           <div className="flex items-center text-secondary-700 text-sm gap-2">
             {getEmptyMessage(sectionType)}
