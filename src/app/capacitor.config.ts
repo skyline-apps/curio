847f3f2215c972f54ddaa4bf165e55221edad911
kimberli <kimberlizhong@gmail.com>
2025-04-23 14:10:21 -0700

    initialize capacitor

---
+++ src/app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "ooo.curi.app",
+  appName: "Curio",
+  webDir: "dist/client",
+};
+
+export default config;
