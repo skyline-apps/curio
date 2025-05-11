0dc161d52e8c98bf3d1dc1725b3041de228c280b
kimberli <kimberlizhong@gmail.com>
2025-05-11 16:39:25 -0700

    add loading for username

--- src/app/components/ui/User/index.tsx
+++ src/app/components/ui/User/index.tsx
@@ -1,9 +1,19 @@
+import { Skeleton } from "@app/components/ui/Skeleton";
 import { User, UserProps } from "@heroui/react";
 
-const CurioUser: React.FC<UserProps> = (props) => {
+const CurioUser: React.FC<UserProps> = ({ name, ...props }: UserProps) => {
+  const loadingName = name ? (
+    <div className="h-5">{name}</div>
+  ) : (
+    <Skeleton className="rounded-lg">
+      <div className="w-32 h-5" />
+    </Skeleton>
+  );
+
   return (
     <User
       {...props}
+      name={loadingName}
       avatarProps={{ classNames: { base: "shrink-0" } }}
       classNames={{ description: "truncate" }}
     />
