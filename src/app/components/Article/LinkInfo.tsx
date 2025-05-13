f5b6ef37aa10bd65d230fb07c647703bfa91f196
kimberli <kimberlizhong@gmail.com>
2025-05-12 23:02:46 -0700

    add long press to tooltip

--- src/app/components/Article/LinkInfo.tsx
+++ src/app/components/Article/LinkInfo.tsx
@@ -17,6 +17,7 @@ const LinkInfo: React.FC<LinkInfoProps> = ({
 
   return (
     <Tooltip
+      longPress={500}
       content={
         <span className="flex items-center gap-1 justify-between max-w-60 p-1 overflow-x-hidden select-none">
           <a
