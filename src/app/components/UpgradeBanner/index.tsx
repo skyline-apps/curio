e3555fc2eaa160b2c231d95d4db9947e2c90431a
kimberli <kimberlizhong@gmail.com>
2025-05-31 20:04:45 -0700

    add upgrade to settings page

--- src/app/components/UpgradeBanner/index.tsx
+++ src/app/components/UpgradeBanner/index.tsx
@@ -1,6 +1,7 @@
 import Button from "@app/components/ui/Button";
 import { useSettings } from "@app/providers/Settings";
 import { HiMiniXMark } from "react-icons/hi2";
+import { useNavigate } from "react-router-dom";
 
 const UPGRADE_PROMPTS = [
   "Support independent software. Upgrade to Curio Premium today.",
@@ -21,9 +22,19 @@ type UpgradeBannerProps = Record<never, never>;
 
 const UpgradeBanner: React.FC<UpgradeBannerProps> = () => {
   const { shouldShowUpgradeBanner, dismissUpgradeBanner } = useSettings();
+  const navigate = useNavigate();
   if (!shouldShowUpgradeBanner) return null;
+
+  const goToUpgradePage = (): void => {
+    navigate("/settings?section=subscription");
+    dismissUpgradeBanner();
+  };
+
   return (
-    <div className="fixed top-0 left-0 right-0 z-50 bg-warning-400 bg-opacity-70 backdrop-blur-md border-b border-warning/40 p-2 flex items-center shadow-xl">
+    <div
+      className="fixed top-0 left-0 right-0 z-50 bg-warning-400 bg-opacity-70 backdrop-blur-md border-b border-warning/40 p-2 flex items-center shadow-xl cursor-pointer"
+      onClick={goToUpgradePage}
+    >
       <div className="flex flex-grow items-center justify-center">
         <p className="text-sm font-medium text-warning-900 drop-shadow-sm">
           {getUpgradePromptOfTheDay()}
