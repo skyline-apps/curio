4c56cc688dc70c0b1a7ce890502f8763c38607be
kimberli <kimberlizhong@gmail.com>
2025-06-15 13:51:49 -0700

    add versionName to context

--- src/app/schemas/v1/premium/item/context/index.ts
+++ src/app/schemas/v1/premium/item/context/index.ts
@@ -1,8 +1,15 @@
 import { z } from "zod";
 
 export const PremiumItemContextRequestSchema = z.object({
-  slug: z.string().min(1),
-  snippet: z.string().min(1),
+  slug: z.string().min(1).describe("The slug of the item."),
+  snippet: z.string().min(1).describe("The text snippet to explain."),
+  versionName: z
+    .string()
+    .max(255)
+    .nullable()
+    .describe(
+      "The version name of the item. If null, the latest version will be used.",
+    ),
 });
 
 export type PremiumItemContextRequest = z.infer<
