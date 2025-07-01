6c8a961cb579fb522308e40cc495d8e4ecbde308
kimberli <kimberlizhong@gmail.com>
2025-06-30 17:45:22 -0700

    fix banner color

--- src/app/utils/cn.ts
+++ src/app/utils/cn.ts
@@ -1,6 +1,11 @@
-import { type ClassValue, clsx } from "clsx";
+import { type ClassValue } from "clsx";
+import { clsx as clsxImpl } from "clsx";
 import { twMerge } from "tailwind-merge"; // eslint-disable-line no-restricted-imports
 
 export function cn(...inputs: ClassValue[]): string {
-  return twMerge(clsx(inputs));
+  return twMerge(clsxImpl(inputs));
+}
+
+export function clsx(...inputs: ClassValue[]): string {
+  return clsxImpl(inputs);
 }
