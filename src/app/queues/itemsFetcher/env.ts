9a1220a4637b12a2e4f8ab3a077641be8091849c
kimberli <kimberlizhong@gmail.com>
2026-06-07 22:22:12 -0700

    fix build

--- src/app/queues/itemsFetcher/env.ts
+++ src/app/queues/itemsFetcher/env.ts
@@ -1,3 +1,4 @@
+import { StorageEnv } from "@app/api/lib/storage";
 import { z } from "zod";
 
 export const EnvSchema = z.object({
@@ -6,8 +7,6 @@ export const EnvSchema = z.object({
   INSTAPAPER_OAUTH_CONSUMER_SECRET: z.string(),
   AXIOM_TOKEN: z.string().optional(),
   AXIOM_DATASET: z.string().optional(),
-  VITE_SUPABASE_URL: z.string(),
-  SUPABASE_SERVICE_ROLE_KEY: z.string(),
 });
 
-export type Env = z.infer<typeof EnvSchema>;
+export type Env = z.infer<typeof EnvSchema> & StorageEnv;
